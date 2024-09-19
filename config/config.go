c788fd963db8c0c34941cd79cc76a9a16a004735
MICHAELKITH <michaelkithinji27@gmail.com>
2024-09-19 23:36:07 +0300

    Implement the configuration for the project

---
+++ config/config.go
@@ -0,0 +1,26 @@
+package config
+
+import (
+    "context"
+    "log"
+    "github.com/jackc/pgx/v5/pgxpool"
+)
+
+var DBPool *pgxpool.Pool
+
+// InitializeDB initializes the database connection pool
+func InitializeDB(dsn string) {
+    var err error
+    DBPool, err = pgxpool.New(context.Background(), dsn)
+    if err != nil {
+        log.Fatalf("Unable to connect to the database: %v\n", err)
+    }
+    log.Println("Database connection established !.")
+}
+
+// CloseDB closes the database connection pool
+func CloseDB() {
+    if DBPool != nil {
+        DBPool.Close()
+    }
+}
