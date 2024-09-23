1f6b5e70aa1b7415e3f1121ba9386e1cc7140a4a
MICHAELKITH <michaelkithinji27@gmail.com>
2024-09-23 23:32:43 +0300

    Implement routes protection

--- routes/routes.go
+++ routes/routes.go
@@ -2,14 +2,20 @@ package routes
 
 import (
 	"github.com/gofiber/fiber/v2"
-	"github.com/savannah/sms/controllers"
+	"github.com/savannah/sms/controllers" // Adjust based on your project structure
 )
 
 func SetupRoutes(app *fiber.App) {
+	app.Get("/", func(c *fiber.Ctx) error {
+		return c.SendString("Welcome to Go Fiber with Okta OpenID Connect!!")
+	})
 
-    app.Post("/customers", controllers.AddCustomer)
-    app.Post("/orders", controllers.AddOrder)
-	app.Put("/orders/:id", controllers.AddOrder)
+	app.Get("/login", controllers.Login)
+	app.Get("/callback", controllers.Callback)
+	app.Post("/users", controllers.CreateUser)
+
+	// Protected routes
+	app.Post("/customers", controllers.Protect, controllers.AddCustomer)
+	app.Post("/orders", controllers.Protect, controllers.AddOrder)
+	app.Put("/orders/:id", controllers.Protect, controllers.AddOrder)
 }
-
-
